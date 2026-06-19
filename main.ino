f169a342f3a77495f2fb0d5559a492823b10ea88
Raiyan K <96327738+Amerax@users.noreply.github.com>
2026-06-19 17:43:48 -0400

    Implement web server for RF light control

--- main.ino
+++ main.ino
@@ -114,3 +114,150 @@ void setup(){
   Serial.println("Time client has started");
 }
 
+void loop(){
+    checkSchedule();
+    WifiClient client = server.available();
+    if(client){
+        Serial.println("New Client");
+        String request = client.readStringUtil('/r');
+        Serial.println("request");
+        client.flush();
+
+        if(request.indexof("/toggle") != -1){
+            sendRF();
+        }
+
+        int timeIndex = request.indexOf("/set?time=");
+
+        if(timeIndex != -1){
+            int start = timeIndex + 10;
+            int end = request.indexOf(' ', start);
+            String tiemValue = request.substring(start, end);
+
+            int encodedIndex = timeValue.indexOf("%3A");
+
+      if (encodedIndex != -1) {
+
+        String hourStr =
+          timeValue.substring(0, encodedIndex);
+
+        String minuteStr = timeValue.substring(encodedIndex + 3); 
+        scheduledHour = hourStr.toInt();
+        scheduledMinute = minuteStr.toInt(); 
+        Serial.print("Setting new time ");
+        Serial.print(scheduledHour); 
+
+        Serial.print(":"); 
+
+        if (scheduledMinute < 10) {
+          Serial.print("0");
+        }
+
+        Serial.println(scheduledMinute);
+      }
+    }
+
+    client.println("Content-type:text/html");
+    client.println();
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>RF Light Control</title>
+<style>
+// basic css nothing special 
+body{
+  background:#111;
+  color:white;
+  font-family:Arial;
+  text-align:center;
+  margin-top:70px;
+}
+
+h1{
+  font-size:50px;
+}
+
+button{
+ width:300px;
+ height:100px;
+  font-size:30px;
+  border:none;
+  border-radius:20px;
+  background:#00cc66;
+  color:white;
+  cursor:pointer;
+  margin-top:20px;
+}
+button:hover{ 
+  background:#00aa55;
+}
+
+input{ 
+  width:220px;
+  height:60px;
+  font-size:28px; 
+  border-radius:15px;
+  border:none;
+  text-align:center;
+}
+
+p{
+  font-size:28px;
+}
+
+</style>
+
+</head>
+
+<body>
+
+<h1>RF Light Control</h1>
+
+<p>Current Auto Time:</p>
+
+<p>
+rawliteral");
+
+    client.print(scheduledHour);
+
+    client.print(":");
+
+   if (scheduledMinute < 10) {
+    client.print("0");
+    }
+
+    client.print(scheduledMinute);
+
+    client.println(R"rawliteral(
+</p>
+
+<form action="/toggle">
+
+<button type="submit">
+Toggle Lights 
+</button>
+</form>
+<form action="/set">
+
+<input type="time" name="time">
+
+<button type="submit">
+Set Auto Time
+</button>
+</form>
+
+</body>
+</html>
+
+)rawliteral");
+    // to get client status
+    client.println();
+    delay(1);
+    client.stop();
+    Serial.println("Client was disconnected!");
+    }
+
+}
